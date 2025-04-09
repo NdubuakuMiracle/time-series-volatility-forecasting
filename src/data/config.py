53a0319fa586af307a91db87eb20932efd18cc8e
Ndubuaku O. Miracle <contact.ndubuaku@gmail.com>
2025-04-09 03:34:49 +0100

    Update config.py

--- src/data/config.py
+++ src/data/config.py
@@ -1,6 +1,37 @@
+"""This module extracts information from the `.env` file so that
+AlphaVantage API key can be used in other parts of the project.
+"""
+
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 import os
 
-class Settings:
-    alpha_api_key = os.getenv("alpha_api_key")
-    
+
+env_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(env_path)
+
+
+class Settings(BaseSettings):
+    """
+    Settings class for configuring the project.
+    This class uses Pydantic's BaseSettings to define and validate the configuration settings
+    required for the project. The settings include:
+    Attributes:
+        alpha_api_key (str): API key for Alpha Vantage.
+    The configuration is loaded from a .env file specified in the inner Config class.
+    Example:
+        To use the settings, create an instance of the Settings class:
+        settings = Settings()
+        print(settings.alpha_api_key)
+    """
+
+    """Uses pydantic to define settings for project."""
+
+    alpha_api_key: str
+
+    class Config:
+        env_file = ".env"
+
+
+# Create instance of `Settings` class that will be imported
 settings = Settings()
